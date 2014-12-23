a424a4c1f1d4bcb69268d4109820e3ed8f6eecf1
Tommy <cchccy1623@gmail.com>
2014-12-23 14:33:34 +0800

    abc

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-Parse.initialize("i3YYpkGy0zHRuBevYamiXHNZIGQO8Mmj7IjUxGXE", "sHviJS2dqoTQWIPM3Fx3Si2zv01YQ9KgMIQXMun5");
+$(document).ready(function(){
+    Parse.initialize("i3YYpkGy0zHRuBevYamiXHNZIGQO8Mmj7IjUxGXE", "sHviJS2dqoTQWIPM3Fx3Si2zv01YQ9KgMIQXMun5");
+    fbLoaded = function () {
+                    FB.Event.subscribe('auth.login', function(response) {
+                        // when user has been logged in, this block will be triggered.
+                        var msg = "You're logged in.";
+                        $("#my-login-message").html(msg);
+                        // print out the response in the console.
+                        fetch_my_profile();
+                    });
+
+                    $("#my-login-button").click(function(){
+                        Parse.FacebookUtils.logIn(null, {
+                          success: function(user) {
+                            if (!user.existed()) {
+                              alert("User signed up and logged in through Facebook!");
+                            } else {
+                              alert("User logged in through Facebook!");
+                            }
+                          },
+                          error: function(user, error) {
+                            alert("User cancelled the Facebook login or did not fully authorize.");
+                          }
+                        });
+                    });
+
+                    $("#logout").click(function(){
+                        Parse.User.logOut();
+                        FB.getLoginStatus(function(response) {
+                            if (response && response.status === 'connected') {
+                                FB.logout(function(response) {
+                                    document.location.reload();
+                                });
+                            }
+                        });
+                    });
+
+                    var fetch_my_profile = function () {
+                        FB.api('/me', function(response) {
+                            var my_name = response.name;
+                            var my_gender = response.gender;
+                            var my_username = response.username;
+                            var my_facebook_id = response.id;
+
+                            $("#my-profile-name").html(my_name);
+                            $("#my-profile-gender").html(my_gender);
+                            $("#my-profile-username").html(my_username);
+                            $("#my-profile-facebook-id").html(my_facebook_id);
+                        });
+
+                        FB.api('/me/picture?width=500', function(response) {
+                            var my_picture_url = response.data.url;
+
+                            $("#my-profile-picture").attr('src', my_picture_url);
+                        });
+                    };
+                }
+})
