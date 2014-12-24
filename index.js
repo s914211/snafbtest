2d25420ec471e5c5759698a103a1b4889c9e7887
Tommy <cchccy1623@gmail.com>
2014-12-24 17:51:33 +0800

    friends

--- index.js
+++ index.js
@@ -1,66 +1,52 @@
 $(document).ready(function(){
     Parse.initialize("i3YYpkGy0zHRuBevYamiXHNZIGQO8Mmj7IjUxGXE", "sHviJS2dqoTQWIPM3Fx3Si2zv01YQ9KgMIQXMun5");
-    fbLoaded = function () {
+    var fetch_my_profile = function () {
+                    FB.api('/me', function(response) {
+                        var my_name = response.name;
+                        var my_gender = response.gender;
+                        var my_username = response.username;
+                        var my_facebook_id = response.id;
 
-        var fetch_my_profile = function () {
-                        FB.api('/me', function(response) {
-                            var my_name = response.name;
-                            var my_gender = response.gender;
-                            var my_username = response.username;
-                            var my_facebook_id = response.id;
-
-                            $("#my-profile-name").html(my_name);
-                            $("#my-profile-gender").html(my_gender);
-                            $("#my-profile-username").html(my_username);
-                            $("#my-profile-facebook-id").html(my_facebook_id);
-                        });
-
-                        FB.api('/me/picture?width=500', function(response) {
-                            var my_picture_url = response.data.url;
-
-                            $("#my-profile-picture").attr('src', my_picture_url);
-                        });
-
-                        FB.api("/me/friends",function (response) {
-                              if (response && !response.error) {
-                                  console.log(response);
-                              }
-                            });
-                    };
-
-                    FB.Event.subscribe('auth.login', function(response) {
-                        // when user has been logged in, this block will be triggered.
-                        var msg = "You're logged in.";
-                        $("#my-login-message").html(msg);
-                        // print out the response in the console.
-                        fetch_my_profile();
+                        $("#my-profile-name").html(my_name);
+                        $("#my-profile-gender").html(my_gender);
+                        $("#my-profile-username").html(my_username);
+                        $("#my-profile-facebook-id").html(my_facebook_id);
                     });
 
-                    $("#my-login-button").click(function(){
-                        Parse.FacebookUtils.logIn("user_friends", {
-                          success: function(user) {
-                            if (!user.existed()) {
-                              alert("User signed up and logged in through Facebook!");
-                            } else {
-                              alert("User logged in through Facebook!");
-                              fetch_my_profile();
-                            }
-                          },
-                          error: function(user, error) {
-                            alert("User cancelled the Facebook login or did not fully authorize.");
-                          }
-                        });
+                    FB.api('/me/picture?width=500', function(response) {
+                        var my_picture_url = response.data.url;
+                        $("#my-profile-picture").attr('src', my_picture_url);
                     });
-
-                    $("#logout").click(function(){
-                        Parse.User.logOut();
-                        FB.getLoginStatus(function(response) {
-                            if (response && response.status === 'connected') {
-                                FB.logout(function(response) {
-                                    document.location.reload();
-                                });
-                            }
-                        });
+                    FB.api("/me/friends",function (response) {
+                        if (response && !response.error) {
+                            console.log(response);
+                        }
                     });
+    };
+    $("#my-login-button").click(function(){
+        Parse.FacebookUtils.logIn("user_friends", {
+            success: function(user) {
+                if (!user.existed()){
+                    alert("User signed up and logged in through Facebook!");
+                } 
+                else{
+                    alert("User logged in through Facebook!");
+                    fetch_my_profile();
                 }
+            },
+            error: function(user, error) {
+                alert("User cancelled the Facebook login or did not fully authorize.");
+            }
+        });
+    });
+    $("#logout").click(function(){
+        Parse.User.logOut();
+        FB.getLoginStatus(function(response) {
+            if (response && response.status === 'connected') {
+                FB.logout(function(response) {
+                    document.location.reload();
+                });
+            }
+        });
+    });
 })
