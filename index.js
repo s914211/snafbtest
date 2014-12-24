e2ee7980ca5520b827598951c62ed27f2ddbe60f
Tommy <cchccy1623@gmail.com>
2014-12-24 18:50:23 +0800

    friends

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ $(document).ready(function(){
                 } 
                 else{
                     console.log(user);
-                    console.log(user.get('authData').get('facebook').get('id'));
+                    console.log(user['authData']['facebook']['id']);
                     alert("User logged in through Facebook!");
                     fetch_my_profile();
                 }
